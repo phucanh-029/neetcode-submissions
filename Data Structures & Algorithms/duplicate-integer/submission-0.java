96fb5de9b5eb40131279558b0d538e200ffaea79
phucanh-029 <65488031+phucanh-029@users.noreply.github.com>
2026-07-01 00:34:16 +0700

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(!hm.containsKey(nums[i])){
+                hm.put(nums[i],0);
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> hm = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            if(!hm.contains(nums[i])){
+                hm.add(nums[i]);
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
