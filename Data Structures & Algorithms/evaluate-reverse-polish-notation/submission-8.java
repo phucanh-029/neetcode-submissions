d326bfaf88cb2e09930b140dc672bc2aae34ca3f
phucanh-029 <65488031+phucanh-029@users.noreply.github.com>
2026-07-29 16:55:26 +0700

    Add: evaluate-reverse-polish-notation - submission-8

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-8.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        int res = 0;
+        Stack<Integer> stk = new Stack<>();
+        int top1=0,top2=0;
+        for(String token : tokens){
+            if(isNum(token)){
+                stk.push(Integer.parseInt(token));
+            }
+            else{
+                switch(token){
+                    case "+":
+                        top1 = stk.pop();
+                        top2 = stk.pop();
+                        // System.out.println(top1+" "+top2);
+                        res = top1+top2;
+                        stk.push(res);
+                        break;
+                    case "-":
+                        top1 = stk.pop();
+                        top2 = stk.pop();
+                        res = top2-top1;
+                        stk.push(res);
+                        break;
+                    case "*":
+                        top1 = stk.pop();
+                        top2 = stk.pop();
+                        res = top1*top2;
+                        // System.out.println(top1+" "+top2);
+                        stk.push(res);
+                        break;
+                    case "/":
+                        top1 = stk.pop();
+                        top2 = stk.pop();
+                        if(top1 ==0 ||top2 ==0)res=0;
+                        else{
+                            res = top2/top1;
+                        }
+                        stk.push(res);
+                        break;
+                    default:
+                        break;
+                }
+                // System.out.println(top1+" "+top2);
+            }
+        }
+        // while(!stk.isEmpty()){
+        //     System.out.println(stk.pop());
+        // }
+        return stk.peek();
+    }
+    private boolean isNum(String s){
+        try{
+            Integer a = Integer.parseInt(s);
+            return true;
+        }catch(NumberFormatException e){
+            return false;
+        }
+    }
+}
