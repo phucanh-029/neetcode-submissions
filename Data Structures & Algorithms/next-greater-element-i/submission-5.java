6f3d1b2c19840652d0e73d17fb2c7544ac72c5a9
phucanh-029 <65488031+phucanh-029@users.noreply.github.com>
2026-07-29 10:58:54 +0700

    Add: next-greater-element-i - submission-5

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-5.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stk = new Stack<>();
+
+        for(int i = nums2.length-1;i>=0;i--){
+            // stk.push(nums2[i]);
+            while(!stk.isEmpty() && nums2[stk.peek()]<=nums2[i]){
+                stk.pop();
+            }
+            for(int j = 0; j < nums1.length; j++){
+                if(nums2[i]==nums1[j]){
+                    nums1[j] = stk.isEmpty() ? -1:nums2[stk.peek()];
+                }
+            }
+            stk.push(i);
+        }
+        // for(int i = 0;i<)
+        return nums1;
+    }
+}
