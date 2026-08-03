fcf43dad77dfd90e4d48a4fbd11131d9019c2373
phucanh-029 <65488031+phucanh-029@users.noreply.github.com>
2026-08-03 16:13:46 +0700

    Add: median-of-two-sorted-arrays - submission-6

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-6.java
@@ -0,0 +1,37 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int len = nums1.length < nums2.length ? (nums1.length + (nums2.length-nums1.length)/2)+1 : (nums2.length + (nums1.length-nums2.length)/2)+1;
+        
+        System.out.println(len);
+        int[] arr = new int[len];
+        int i = 0,j = 0,idx = 0;
+        int sum = nums1.length + nums2.length;
+        while(i+j<len){
+            System.out.println(i+" "+j+" idx "+idx);
+            if(nums1.length==0)arr[idx++] = nums2[j++];
+            else if(nums2.length==0)arr[idx++] = nums1[i++];
+            else {
+                if(nums1[i]< nums2[j]){
+                    arr[idx++]=nums1[i];
+                    i++;
+                    if(i==nums1.length)break;
+                }
+                else{
+                    arr[idx++]= nums2[j];
+                    j++;
+                    if(j==nums2.length)break;
+                }
+            }
+            
+        }
+        if(i==nums1.length) while(j<len-i)arr[idx++]=nums2[j++];
+        if(j==nums2.length) while(i<len-j)arr[idx++]=nums1[i++];
+        for(int n:arr)System.out.println(n);
+        // if(nums1.length == 0 || nums2.length ==0) {
+        //     if(len == 2) return (double)(arr[len-1]+arr[len-2])/2;
+        //     else if(len%2==0 || len ==1) return arr[len-1];
+        //     else return (double)(arr[len-1]+arr[len-2])/2;
+        // }
+        return sum%2==1 ? arr[len-1]:(double)(arr[len-1]+arr[len-2])/2;
+    }
+}
