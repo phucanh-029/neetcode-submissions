4d711d8e72662158509c9a29976039a865f9c21a
phucanh-029 <65488031+phucanh-029@users.noreply.github.com>
2026-08-18 16:06:07 +0700

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1) return 1;
+        if(n==2)return 2;
+        int[] stair = new int[n];
+        stair[0]=1;
+        stair[1]=2;
+        for(int i = 2; i < n; i++){
+            stair[i] = stair[i-1]+stair[i-2];
+        }
+        return stair[n-1];
+    }
+}
