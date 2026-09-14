a0212bf8ee98e649afe41c02563ae4e8fc68e2cc
phucanh-029 <65488031+phucanh-029@users.noreply.github.com>
2026-09-14 14:51:45 +0700

    Add: meeting-schedule-ii - submission-5

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-5.java
@@ -0,0 +1,28 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        TreeMap<Integer,Integer> tm = new TreeMap<>();
+        for(Interval i : intervals){
+            tm.put(i.start, tm.getOrDefault(i.start,0)+1);
+            tm.put(i.end, tm.getOrDefault(i.end,0)-1);
+        }
+        int prev = 0;
+        int res = 0;
+        for(Integer key : tm.keySet()){
+            prev+=tm.get(key);
+            res = Math.max(res,prev);
+        }
+
+        return res;
+    }
+}
